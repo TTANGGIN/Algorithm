e6fa9fc8defefa24b0a3e618520d1c3aeca92499
TTANGGIN <58355254+TTANGGIN@users.noreply.github.com>
2023-04-07 02:16:31 +0900

    solved beakjoon 15654

---
+++ beakjoon/_15654/Main.java
@@ -0,0 +1,54 @@
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N;
+    static int M;
+    static int[] numbers;
+    static int[] arr;
+    static boolean[] visited;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[M];
+        visited = new boolean[N];
+        numbers = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            numbers[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(numbers);
+
+        sequence(0);
+        System.out.format("%s", sb.toString());
+    }
+
+    public static void sequence(int index) {
+        if (index == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(arr[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                arr[index] = numbers[i];
+                sequence(index + 1);
+                visited[i] = false;
+            }
+        }
+    }
+}
