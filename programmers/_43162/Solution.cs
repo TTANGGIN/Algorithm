6272914ef774f06f68a1ae8239a0fdb5cd66fb8c
TTANGGIN <58355254+TTANGGIN@users.noreply.github.com>
2023-05-09 23:27:41 +0900

    solved programmers 43162

---
+++ programmers/_43162/Solution.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+public class Solution
+{
+    int n;
+    bool[] visited;
+    int[,] computers;
+    public int solution(int n, int[,] computers)
+    {
+        this.n = n;
+        this.computers = computers;
+        visited = new bool[n];
+
+        int answer = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (visited[i]) continue;
+            DFS(i);
+            answer++;
+        }
+            
+        return answer;
+    }
+
+    private void DFS(int index)
+    {
+        visited[index] = true;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (!visited[i] && computers[index, i] == 1)
+            {
+                DFS(i);
+            }
+        }
+    }
+}
