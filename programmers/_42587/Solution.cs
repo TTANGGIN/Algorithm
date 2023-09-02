cf6f3c3b1d57714888359ba2b1ade6647a9eeda0
TTANGGIN <58355254+TTANGGIN@users.noreply.github.com>
2023-09-02 19:06:39 +0900

    solved programmers 42587

---
+++ programmers/_42587/Solution.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Solution
+{
+    public int solution(int[] priorities, int location)
+    {
+        int answer = 0;
+        Queue<(int priority, int index)> queue = new Queue<(int priority, int index)>();
+
+        for (int i = 0; i < priorities.Length; i++)
+        {
+            queue.Enqueue((priorities[i], i));
+        }
+
+        while (queue.Count > 0)
+        {
+            var current = queue.Dequeue();
+            if (queue.Count > 0 && current.priority < queue.Max(item => item.priority))
+            {
+                queue.Enqueue(current);
+            }
+            else
+            {
+                answer++;
+                if (current.index == location)
+                {
+                    break;
+                }
+            }
+        }
+        return answer;
+    }
+}
