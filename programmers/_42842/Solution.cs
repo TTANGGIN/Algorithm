437cf2b874d2190c4ffd4951c4f535ed9530ad6c
TTANGGIN <58355254+TTANGGIN@users.noreply.github.com>
2023-09-11 22:28:36 +0900

    solved programmers 42842

---
+++ programmers/_42842/Solution.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class Solution
+{
+    public int[] solution(int brown, int yellow)
+    {
+        int[] answer = new int[2];
+        int sqrtY = (int)Math.Sqrt(yellow);
+
+        for (int i = 1; i <= sqrtY; i++)
+        {
+            if (yellow % i == 0)
+            {
+                int b = yellow / i;
+                int perimeters = 2 * (i + b) + 4;
+                if (brown == perimeters)
+                {
+                    answer[0] = Math.Max(i, b) + 2;
+                    answer[1] = Math.Min(i, b) + 2;
+                    break;
+                }
+            }
+        }
+
+        return answer;
+    }
+}
