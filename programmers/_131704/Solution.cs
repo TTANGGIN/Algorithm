7b6c0f496be3cfd02954307b16ecace72cb1011b
TTANGGIN <58355254+TTANGGIN@users.noreply.github.com>
2023-09-19 16:25:03 +0900

    solved programmers 131704

---
+++ programmers/_131704/Solution.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Solution
+{
+    public int solution(int[] order)
+    {
+        int answer = 0;
+        int peek = 0;
+        Stack<int> subBelt = new Stack<int>();
+
+        for (int i = 1; i <= order.Length; i++)
+        {
+            bool isLoaded = false;
+
+            if (order[answer] == i)
+            {
+                answer++;
+                isLoaded = true;
+            }
+            while (subBelt.TryPeek(out peek) && peek == order[answer])
+            {
+                subBelt.Pop();
+                answer++;
+                isLoaded = true;
+            }
+
+            if (!isLoaded)
+            {
+                subBelt.Push(i);
+            }
+
+        }
+
+        while (subBelt.TryPeek(out peek) && peek == order[answer])
+        {
+            subBelt.Pop();
+            answer++;
+        }
+
+        return answer;
+    }
+}
